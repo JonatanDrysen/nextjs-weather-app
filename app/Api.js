06de5624ab42839c566177fedead81293513f6fb
JonatanDrysen <jojodrysen@gmail.com>
2024-04-17 17:35:39 +0200

    update import/export

--- app/Api.js
+++ app/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 import "dotenv/config"
 
-async function fetchWeather() {
+export default async function fetchWeather() {
     const API_KEY = process.env.NEXT_PUBLIC_API_KEY
     const url = `https://api.openweathermap.org/data/3.0/onecall?lat=59.3&lon=17.8&exclude=minutely,hourly,daily&appid=${API_KEY}&units=metric`
 
@@ -15,5 +15,3 @@ async function fetchWeather() {
         return false
     }
 }
-
-export default fetchWeather
